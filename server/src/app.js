cfbfd7fbca438125c5b4253b951fbe862648254f
Prak005 <prakharmallick005@gmail.com>
2026-05-10 16:51:15 +0530

    initialise react frontend and backend set up

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(express.json());
+
+app.use(
+    cors({
+        origin: "http://localhost:5173",
+        credentials: true,
+    })
+);
 
 app.get('/', (req, res) => {
     res.send("Server running");
