14d38edada28904149f740a8e2853e8c929fe9a0
Prak005 <prakharmallick005@gmail.com>
2026-05-12 17:49:28 +0530

    connect react frontend to authentication backend

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,9 +1,45 @@
+import {useState} from "react";
+import api from "../api/axios";
+
 function LoginPage () {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try{
+            const response = await api.post("/auth/login", {
+                username,
+                password,
+            });
+            console.log(response.data);
+        } catch(error) {
+            console.log(error.response.data);
+        }
+    }
     return (
         <div className="p-8">
             <h1 className="text-3xl font-bold">
                 Login Page
             </h1>
+            <form onSubmit={handleSubmit} className="flex flex-col gap-4 max-w-sm">
+                <input
+                    type="text"
+                    placeholder="Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    className="border p-2 rounded"
+                />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="border p-2 rounded"
+                />
+                <button type="submit" className="border p-2 rounded">
+                    Login
+                </button>
+            </form>
         </div>
     );
 }
