726956999fda2a6ac44741f34d3cbbb6093200eb
Prak005 <prakharmallick005@gmail.com>
2026-05-13 01:28:13 +0530

    fix dir missing issue

--- server/src/config/multer.js
+++ server/src/config/multer.js
@@ -1,5 +1,9 @@
 const multer = require("multer");
+const fs = require("fs");
 
+if (!fs.existsSync("uploads")) {
+    fs.mkdirSync("uploads");
+}
 const storage = multer.diskStorage({
     destination: function(req, file, cb){
         cb(null, "uploads/");
