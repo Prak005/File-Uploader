58c1d8230e8cb99850994f59c860eb758b693bde
Prak005 <prakharmallick005@gmail.com>
2026-05-24 18:34:20 +0530

    add file size and type restrictions

--- server/src/config/multer.js
+++ server/src/config/multer.js
@@ -15,6 +15,21 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage,
+    limits: {
+        fileSize: 5*1024*1024,
+    },
+    fileFilter: (req, file, cb) => {
+        const allowedTypes = [
+            "image/png",
+            "image/jpeg",
+            "application/pdf",
+        ];
+        if(allowedTypes.includes(file.mimetype)){
+            cb(null, true);
+        } else {
+            cb(new Error("Invalid file type"));
+        }
+    },
 });
 
 module.exports = upload;
